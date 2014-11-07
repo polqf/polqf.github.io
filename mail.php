28b8f83a8b736729e1dcfabf0805e82af1865a7a
Pol Quintana <polkintana@gmail.com>
2014-11-07 11:48:18 +0100

    Test

--- mail.php
+++ mail.php
@@ -8,6 +8,6 @@ $recipient = "pol.quintana1@gmail.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader);
-echo "Thank You!";
+echo "Thank You!" . " -" . "<a href='form.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
 
 ?>
